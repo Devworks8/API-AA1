7baf71cee8d8c3bc027292dd34508347fa70545f
Devworks8 <lachapellec@gmail.com>
2021-11-06 18:17:23 -0400

    Minor changes: replaced hard values with variables

--- Assignment1.py
+++ Assignment1.py
@@ -7,30 +7,31 @@ import pin_controller as pctrl
 
 
 def run_question_one():
+    period = 1  # Seconds
     # Turn all LEDs ON for 1 second
     pctrl.all_on()
-    time.sleep(1)
+    time.sleep(period)
     # Turn all LEDs OFF for 1 second
     pctrl.all_off()
-    time.sleep(1)
+    time.sleep(period)
     # Turn LED a ON for 1 second
     pctrl.pin_on(22)
-    time.sleep(1)
+    time.sleep(period)
     # Turn LED a OFF for 1 second
     pctrl.pin_off(22)
-    time.sleep(1)
+    time.sleep(period)
     # Turn LED b ON for 1 second
     pctrl.pin_on(22)
-    time.sleep(1)
+    time.sleep(period)
     # Turn LED b OFF for 1 second
     pctrl.pin_off(22)
-    time.sleep(1)
+    time.sleep(period)
     # Turn LED c ON for 1 second
     pctrl.pin_on(22)
-    time.sleep(1)
+    time.sleep(period)
     # Turn LED c OFF for 1 second
     pctrl.pin_off(22)
-    time.sleep(1)
+    time.sleep(period)
     # Run the stobe_reg method
     pctrl.strobe_reg()
     # Turn all LEDs OFF when keyboard interrupt is received before proceeding
@@ -38,21 +39,24 @@ def run_question_one():
 
 
 def run_question_two():
+    on_period = 3  # Seconds
+    off_period = 2  # Seconds
+    iterations = 3
     url = 'http://localhost:5000/pins/1'
     body_on_state = "{\"pin_num\": 22, \"color\": \"red\", \"state\": \"on\"}"
     body_off_state = "{\"pin_num\": 22, \"color\": \"red\", \"state\": \"off\"}"
     headers = CaseInsensitiveDict()
     headers["Content-Type"] = "application/json"
 
-    for i in range(4):
+    for i in range(iterations):
         resp = requests.put(url, headers=headers, data=body_on_state)
-        print("Turning ON for 3 seconds...")
+        print(f"Turning ON for {on_period} seconds...")
         print(f"Status Code: {resp.status_code}")
-        time.sleep(3)
+        time.sleep(on_period)
         resp = requests.put(url, headers=headers, data=body_off_state)
-        print("Turning OFF for 1 seconds...")
+        print(f"Turning OFF for {off_period} seconds...")
         print(f"Status Code: {resp.status_code}")
-        time.sleep(1)
+        time.sleep(off_period)
 
 
 if __name__ == '__main__':
